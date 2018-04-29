a0dfaf8b73c8e114e655963b8d71c862666768e5
Kyungman Kim <gracefulife@naver.com>
2018-04-29 15:37:01 +0900

    init commit

---
+++ src/main/java/com/depromeet/donkey/DonkeyApplication.java
@@ -0,0 +1,12 @@
+package com.depromeet.donkey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonkeyApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DonkeyApplication.class, args);
+  }
+}
