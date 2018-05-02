3f3f1618a2b2b1589171e83b402e349eec4603a6
gracefulife <gracefulife@naver.com>
2018-05-02 18:00:05 +0900

    의존성 조정 및 멀티트랜잭션매니저 활용을 위한 설정파일 조정

---
+++ src/main/java/com/depromeet/donkey/DonkeyDatabaseConfig.java
@@ -0,0 +1,45 @@
+package com.depromeet.donkey;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableJpaRepositories(
+    entityManagerFactoryRef = "donkeyEntityManagerFactory",
+    transactionManagerRef = "donkeyTransactionManager",
+    basePackages = "com.depromeet.donkey.entity"
+)
+public class DonkeyDatabaseConfig {
+
+  @Bean
+  @ConfigurationProperties(prefix = "spring.datasource.donkey")
+  public DataSource donkeyDataSource() {
+    return DataSourceBuilder.create().build();
+  }
+
+  @Bean
+  public LocalContainerEntityManagerFactoryBean donkeyEntityManagerFactory() {
+    HibernateJpaVendorAdapter jpaVendorAdapter = new HibernateJpaVendorAdapter();
+    LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+
+    factoryBean.setDataSource(donkeyDataSource());
+    factoryBean.setJpaVendorAdapter(jpaVendorAdapter);
+    factoryBean.setPackagesToScan("com.depromeet.donkey.entity");
+
+    return factoryBean;
+  }
+
+  @Bean
+  public PlatformTransactionManager donkeyTransactionManager() {
+    return new JpaTransactionManager(donkeyEntityManagerFactory().getObject());
+  }
+}
